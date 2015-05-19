6aa72d39ad703c4956e7a0708d9350c90f1bb997
taijuios <xianlin@xianlindeiMac.local>
2015-05-19 09:36:32 +0800

    Initial Commit

---
+++ vlifree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  vlifree
+//
+//  Created by 仙林 on 15/5/19.
+//  Copyright (c) 2015年 仙林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
